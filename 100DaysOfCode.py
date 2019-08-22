3b3a089d5c1c3d62311a17cc84538b91382b2f56
thameralbalawi <thameralbalawi400415@gmail.com>
2019-08-22 22:18:04 +0300

    enter

--- 100DaysOfCode.py
+++ 100DaysOfCode.py
@@ -8,3 +8,12 @@ this is a comment
 written in 
 more than just one line
 """
+# Day 3
+x = 4 # x is of type int
+x = "Sally"  # x is now of type str
+print(x)
+########################
+x = "Python is"
+y = "awesome"
+z = x + y
+print(z)
