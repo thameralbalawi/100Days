b8d8255d61b7bfd72419670e12769944ce964dab
thameralbalawi <thameralbalawi400415@gmail.com>
2019-08-23 17:45:14 +0300

    enter

--- 100DaysOfCode.py
+++ 100DaysOfCode.py
@@ -4,12 +4,14 @@ import random
 print("Hello, World!")
 if 5 > 2:
     print("Five is greater than two!")
+
 # Day 2
 """
 this is a comment 
 written in 
 more than just one line
 """
+
 # Day 3
 ######## exercise 1 #########
 x = 4 # x is of type int
@@ -20,6 +22,7 @@ x = "Python is "
 y = "awesome"
 z = x + y
 print(z)
+
 # Day 4
 x = 1 # int
 y = 208 #float
