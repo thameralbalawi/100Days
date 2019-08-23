00474c6adf7d36099f0543e1ac04378a9da2ce14
thameralbalawi <thameralbalawi400415@gmail.com>
2019-08-23 17:44:09 +0300

    enter

--- 100DaysOfCode.py
+++ 100DaysOfCode.py
@@ -1,3 +1,5 @@
+import random
+
 # Day 1
 print("Hello, World!")
 if 5 > 2:
@@ -14,7 +16,28 @@ x = 4 # x is of type int
 x = "Sally"  # x is now of type str
 print(x)
 ######## exercise 2 #########
-x = "Python is"
+x = "Python is "
 y = "awesome"
 z = x + y
 print(z)
+# Day 4
+x = 1 # int
+y = 208 #float
+z = 1j # complex
+
+#conver from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
